9d16bb9aa6ed318c7730248a88fe579bb5cfcfd2
Misheel31 <misheelrai7@gmail.com>
2024-03-01 11:38:36 +0545

    All updated

--- src/main/java/com/example/grocerystore/service/AuthenticateService.java
+++ src/main/java/com/example/grocerystore/service/AuthenticateService.java
@@ -5,8 +5,5 @@ import com.example.grocerystore.dto.AuthenticateResponse;
 
 public interface AuthenticateService {
     AuthenticateResponse authenticate(AuthenticateRequest authenticateRequest);
-    void initiateForgetPassword(String email);
-    boolean validateForgetPasswordOtp(String email, String otp);
-    void updatePassword(String email, String newPassword);
 }
  
